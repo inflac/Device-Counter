4340e1a0dac790e8ae1a60a73ab1071d39a83b2b
inflac <inflac@inflacsan.de>
2022-06-16 20:18:21 +0200

    Update wifi_start_scan.sh
    Skript uses the last attached wifi adapter for the scan

--- device_counter/wifi_start_scan.sh
+++ device_counter/wifi_start_scan.sh
@@ -1,2 +1,4 @@
-airmon-ng start wlan1
+WLAN=$(ip link show | awk '{print $2}' | grep "wlan" | sort -k 1,1 | tail -1)
+WLAN=${WLAN%?}
+airmon-ng start $WLAN
 airodump-ng --berlin 60000 -w /home/kali/Desktop/device_counter/wififinder/WIFICapture --channel 1-13,36-165 --write-interval 10 --output-format csv wlan1mon
